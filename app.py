d9be5eb872d0a5bd23685b13be3301c53c428023
ycsara <ycsara2020@gmail.com>
2025-02-22 10:15:17 -0800

    new

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ import numpy as np
 import plotly.express as px
 
 st.title('Analysis of Sleep Cycle Productivity')
-st.write('Filter the data below to see the ads by manufacturer')
 
 sleep = pd.read_csv('sleep_cycle_productivity.csv', sep=',')
 
-mood_choice = range(1, 11)
-st.selectbox('Select a Mood Score', mood_choice)
+st.write('This graph will analyze how much the amount of sleep can affect productivity, mood, and stress.')
+fig1 = px.histogram(sleep, x='Total Sleep Hours')
+
+st.title('Analysis of Sleep Cycle Productivity')
