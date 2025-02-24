84fda299dbd75f18efa65ebf2527a44698368a72
ycsara <ycsara2020@gmail.com>
2025-02-24 10:45:42 -0800

    change

--- app.py
+++ app.py
@@ -10,19 +10,21 @@ sh = pd.read_csv('Sleep_health_and_lifestyle_dataset.csv', sep=',')
 st.subheader('Difference in Lifestyle Factors')
 st.write('The bar graph and scatterplot show health effects based on occupation and sleep disorders')
     
-choose = ['Sleep Duration', 'Quality of Sleep', 'Physical Activity Level', 
+choose1 = ['Sleep Duration', 'Quality of Sleep', 'Physical Activity Level', 
           'Stress Level', 'Heart Rate', 'Daily Steps']
-selected_type = st.selectbox('Choose a variable to visualize', choose)
-sleep_occ = sh.groupby('Occupation')[selected_type].mean().reset_index()
-fig1 = px.bar(sleep_occ, x='Occupation', y=selected_type, color=selected_type,
+selected_type1 = st.selectbox('Choose a variable to visualize', choose1)
+sleep_occ = sh.groupby('Occupation')[selected_type1].mean().reset_index()
+fig1 = px.bar(sleep_occ, x='Occupation', y=selected_type1, color=selected_type1,
               color_continuous_scale='redor')
-fig1.update_layout(title=f'<b> Average {selected_type} by Occupation</b>')
+fig1.update_layout(title=f'<b> Average {selected_type1} by Occupation</b>')
 st.plotly_chart(fig1)
 
+choose2 = ['Sleep Duration', 'Physical Activity Level', 'Stress Level', 'Heart Rate', 'Daily Steps']
+selected_type2 = st.selectbox('Choose a variable to visualize', choose2)
 sh['Sleep Disorder'].fillna('None', inplace=True)
-fig2 = px.scatter(sh,x='Quality of Sleep', y=selected_type, color='Sleep Disorder', 
+fig2 = px.scatter(sh,x='Quality of Sleep', y=selected_type2, color='Sleep Disorder', 
                   size_max=13, trendline='ols')
-fig2.update_layout(title=f'<b> Quality of Sleep vs {selected_type} by Sleep Disorders </b>')
+fig2.update_layout(title=f'<b> Quality of Sleep vs {selected_type2} by Sleep Disorders </b>')
 st.plotly_chart(fig2)
 
 st.subheader('Group by Gender')
