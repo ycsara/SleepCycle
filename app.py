fb053880ca4a306d749eec6b5b3fe95156764737
ycsara <ycsara2020@gmail.com>
2025-02-24 03:13:39 -0800

    new

--- app.py
+++ app.py
@@ -3,16 +3,39 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 
-st.header('Analysis of Sleep Cycle Productivity')
+st.title('Sleep Health Data Analysis')
 
-sleep = pd.read_csv('Sleep_health_and_lifestyle_dataset.csv', sep=',')
+sh = pd.read_csv('Sleep_health_and_lifestyle_dataset.csv', sep=',')
 
-st.write('This graph will analyze how much sleep quality can affect productivity, mood, and stress.')
-
-score = ['Productivity Score', 'Mood Score', 'Stress Level']
-selected_type = st.selectbox('Choose a variable', score)
-#sleep = sleep.sort_values(by='Sleep Quality')
-fig1 = px.histogram(sleep, x='Sleep Quality', color=selected_type, nbins=20)
-fig1.update_layout(title='<b> Distribution of Sleep Quality by {}</b>'.format(selected_type))
-fig1.update_yaxes(categoryorder='category ascending')
+st.subheader('Difference in Lifestyle Factors')
+st.write('The bar graph and scatterplot show health effects based on occupation and sleep disorders')
+    
+choose = ['Sleep Duration', 'Quality of Sleep', 'Physical Activity Level', 
+          'Stress Level', 'Heart Rate', 'Daily Steps']
+selected_type = st.selectbox('Choose a variable to visualize', choose)
+sleep_occ = sh.groupby('Occupation')[selected_type].mean().reset_index()
+fig1 = px.bar(sleep_occ, x='Occupation', y=selected_type, color=selected_type,
+              color_continuous_scale='redor')
+fig1.update_layout(title=f'<b> Average {selected_type} by Occupation</b>')
 st.plotly_chart(fig1)
+
+sh['Sleep Disorder'].fillna('None', inplace=True)
+fig2 = px.scatter(sh,x='Quality of Sleep', y=selected_type, color='Sleep Disorder', 
+                  size_max=13, trendline='ols')
+fig2.update_layout(title=f'<b> Quality of Sleep vs {selected_type} by Sleep Disorders </b>')
+st.plotly_chart(fig2)
+
+st.subheader('Group by Gender')
+st.write('The histogram displays the distribution grouped by gender.')
+
+gender = []
+
+box = st.checkbox('Click to group by gender')
+if box:
+    fig3 = px.histogram(sh, x='Sleep Duration', nbins=20, color='Gender',
+                        barmode='overlay', title='<b>Sleep Duration by Gender</b>',
+                        labels={'Sleep Duration': 'Sleep Duration (hours)'})
+else:
+    fig = px.histogram(sh, x='Sleep Duration', title='<b>Sleep Duration</b>',
+                       labels={'Sleep Duration': 'Sleep Duration (hours)'}, nbins=20)
+st.plotly_chart(fig3)
